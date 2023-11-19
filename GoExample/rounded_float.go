d5111f3ecba9eafe10c53e01e30390b4b6811e38
D Drozdov <dsdrozd2@mts.ru>
2023-11-19 18:32:33 +0300

    improve print

--- GoExample/rounded_float.go
+++ GoExample/rounded_float.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 type RoundedFloat float64
 
 func (r RoundedFloat) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf("%.1f", r)), nil
+	str := fmt.Sprintf("%.2f", r)
+	str = strings.TrimRight(str, "0")
+	str = strings.TrimRight(str, ".")
+	return []byte(str), nil
 }
