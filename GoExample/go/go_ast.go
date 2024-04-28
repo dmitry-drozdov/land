eb782711de58444e49f6db74c0036c958b6ff615
D Drozdov <dsdrozd2@mts.ru>
2024-04-29 01:16:17 +0300

    fix ast nodes

--- GoExample/go/go_ast.go
+++ GoExample/go/go_ast.go
@@ -32,6 +32,9 @@ func ParseFiles(root string) (map[string]map[string]*FuncStat, map[string]map[st
 	var m1, m2 runtime.MemStats
 	runtime.ReadMemStats(&m1)
 
+	totalRequiredNodes := 0
+	totalNodes := 0
+
 	// alreadyDone := make(map[uint64]struct{}, 10000)
 	// m := sync.Mutex{}
 
@@ -70,7 +73,7 @@ func ParseFiles(root string) (map[string]map[string]*FuncStat, map[string]map[st
 
 		g.Go(func() error {
 			t0 := time.Now()
-			f, dataFunc, dataStruct, err := ParseFile(pathBk)
+			nodesReq, nodesAll, f, dataFunc, dataStruct, err := ParseFile(pathBk)
 			if err != nil {
 				return err
 			}
@@ -83,6 +86,8 @@ func ParseFiles(root string) (map[string]map[string]*FuncStat, map[string]map[st
 			resFun[pathBk] = dataFunc
 			resStruct[pathBk] = dataStruct
 			timeSpent += int64(time.Since(t0))
+			totalNodes += nodesAll
+			totalRequiredNodes += nodesReq
 			l.Unlock()
 			return nil
 		})
@@ -105,12 +110,12 @@ func ParseFiles(root string) (map[string]map[string]*FuncStat, map[string]map[st
 
 	fmt.Printf("%.2f sec.\n", float64(timeSpent)/(float64(time.Second)))
 	fmt.Printf("%d Mb \n", maxMemory/(1024*1024))
-	fmt.Printf("%d ast objs\n", cnt(files))
+	fmt.Printf("%d/%d req/all nodes (%.2f%%)\n", totalRequiredNodes, totalNodes, ratio(totalRequiredNodes, totalNodes))
 
 	return resFun, resStruct, duplicates, nil
 }
 
-func ParseFile(path string) (*ast.File, map[string]*FuncStat, map[string]*StructStat, error) {
+func ParseFile(path string) (int, int, *ast.File, map[string]*FuncStat, map[string]*StructStat, error) {
 	resFun := make(map[string]*FuncStat, 100)
 	resStruct := make(map[string]*StructStat, 100)
 
@@ -118,7 +123,7 @@ func ParseFile(path string) (*ast.File, map[string]*FuncStat, map[string]*Struct
 
 	f, err := parser.ParseFile(fset, path, nil, 0)
 	if err != nil {
-		return nil, nil, nil, err
+		return 0, 0, nil, nil, nil, err
 	}
 
 	type pair struct{ start, end token.Pos }
@@ -133,7 +138,11 @@ func ParseFile(path string) (*ast.File, map[string]*FuncStat, map[string]*Struct
 		return false
 	}
 
+	allCnt := 0
+	reqCnt := 0
+
 	ast.Inspect(f, func(n ast.Node) bool {
+		allCnt++
 		if n != nil && checkInside(n.Pos(), n.End()) {
 			return true
 		}
@@ -142,13 +151,14 @@ func ParseFile(path string) (*ast.File, map[string]*FuncStat, map[string]*Struct
 		case *ast.TypeSpec:
 			switch y := x.Type.(type) {
 			case *ast.StructType:
+				reqCnt++
 				resStruct[x.Name.Name] = &StructStat{Name: x.Name.Name, Types: make([]string, 0, 5)}
 				for _, f := range y.Fields.List {
 					for i := 0; i < len(f.Names); i++ { // a, b, c int => append 3 int
-						resStruct[x.Name.Name].Types = append(resStruct[x.Name.Name].Types, HumanType(f.Type))
+						resStruct[x.Name.Name].Types = append(resStruct[x.Name.Name].Types, HumanType(f.Type, &reqCnt))
 					}
 					if len(f.Names) == 0 { // embedded type
-						resStruct[x.Name.Name].Types = append(resStruct[x.Name.Name].Types, HumanType(f.Type))
+						resStruct[x.Name.Name].Types = append(resStruct[x.Name.Name].Types, HumanType(f.Type, &reqCnt))
 					}
 				}
 			}
@@ -167,6 +177,8 @@ func ParseFile(path string) (*ast.File, map[string]*FuncStat, map[string]*Struct
 			}
 			resFun[x.Name.Name] = ptr
 
+			reqCnt += 1 + int(ret)
+
 			add := func(a string) {
 				ptr.Args = append(ptr.Args, a)
 			}
@@ -174,11 +186,11 @@ func ParseFile(path string) (*ast.File, map[string]*FuncStat, map[string]*Struct
 			for _, y := range x.Type.Params.List {
 				// can be several args with 1 type: n int, j, k, l float
 				for i := 0; i < len(y.Names); i++ {
-					add(HumanType(y.Type))
+					add(HumanType(y.Type, &reqCnt))
 				}
 				// at least 1 name always presented
 				if len(y.Names) == 0 {
-					add(HumanType(y.Type))
+					add(HumanType(y.Type, &reqCnt))
 				}
 				// only types (=0) or mixed only types + name with type (=1)
 				if len(y.Names) != 1 {
@@ -197,25 +209,26 @@ func ParseFile(path string) (*ast.File, map[string]*FuncStat, map[string]*Struct
 		return true
 	})
 
-	return f, resFun, resStruct, nil
+	return reqCnt, allCnt, f, resFun, resStruct, nil
 }
 
-func HumanType(tp ast.Expr) string {
+func HumanType(tp ast.Expr, reqCnt *int) string {
+	*reqCnt++
 	switch t := tp.(type) {
 	case *ast.Ident:
 		return t.Name
 	case *ast.SelectorExpr:
 		return fmt.Sprintf("%v.%v\n", t.X, t.Sel.Name)
 	case *ast.StarExpr:
-		return HumanType(t.X)
+		return HumanType(t.X, reqCnt)
 	case *ast.ArrayType:
-		return HumanType(t.Elt)
+		return HumanType(t.Elt, reqCnt)
 	case *ast.IndexExpr:
-		return HumanType(t.X)
+		return HumanType(t.X, reqCnt)
 	case *ast.IndexListExpr:
-		return HumanType(t.X)
+		return HumanType(t.X, reqCnt)
 	case *ast.ParenExpr:
-		return HumanType(t.X)
+		return HumanType(t.X, reqCnt)
 	case *ast.FuncType:
 		return "anon_func_title"
 	case *ast.ChanType:
@@ -227,18 +240,7 @@ func HumanType(tp ast.Expr) string {
 	case *ast.InterfaceType:
 		return "anon_interface"
 	case *ast.Ellipsis:
-		return HumanType(t.Elt)
+		return HumanType(t.Elt, reqCnt)
 	}
 	return fmt.Sprintf("%T", tp)
-}
-
-func cnt(files []*ast.File) int {
-	res := 0
-	for _, f := range files {
-		res += len(f.Comments)
-		res += len(f.Decls)
-		res += len(f.Imports)
-		res += len(f.Unresolved)
-	}
-	return res
 }
