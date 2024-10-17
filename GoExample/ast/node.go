056bf92b4d318f13e1f23cc43b56b08eb10e345c
Дроздов Дмитрий Сергеевич <dsdrozd2@mts.ru>
2024-10-18 00:23:42 +0300

    тестирование слияния деревьев

---
+++ GoExample/ast/node.go
@@ -0,0 +1,129 @@
+package main
+
+import "sort"
+
+type Offset struct {
+	Start int
+	End   int
+}
+
+func (o Offset) Contains(other Offset) bool {
+	return o.Start <= other.Start && o.End >= other.End
+}
+
+func (o Offset) Overlaps(other Offset) bool {
+	return o.Start < other.End && o.End > other.Start
+}
+
+type Node struct {
+	Offs    Offset
+	Chldren []*Node
+}
+
+func MergeTrees(node1, node2 *Node) *Node {
+	if node1 == nil {
+		return node2
+	}
+	if node2 == nil {
+		return node1
+	}
+
+	if node1.Offs == node2.Offs {
+		return &Node{
+			Offs:    node1.Offs,
+			Chldren: MergeChildLists(node1.Chldren, node2.Chldren),
+		}
+	}
+
+	if node1.Offs.Contains(node2.Offs) {
+		node1.Chldren = MergeChildIntoChildren(node1.Chldren, node2)
+		return node1
+	}
+
+	if node2.Offs.Contains(node1.Offs) {
+		node2.Chldren = MergeChildIntoChildren(node2.Chldren, node1)
+		return node1
+	}
+
+	// Обработка перекрывающихся узлов
+	if node1.Offs.Overlaps(node2.Offs) {
+		mergedNode := &Node{
+			Offs: Offset{
+				Start: min(node1.Offs.Start, node2.Offs.Start),
+				End:   max(node1.Offs.End, node2.Offs.End),
+			},
+			Chldren: []*Node{node1, node2},
+		}
+		//mergedNode.Children.Sort((a, b) => a.Offset.Start.CompareTo(b.Offset.Start));
+		sort.Slice(mergedNode.Chldren, func(i, j int) bool {
+			return mergedNode.Chldren[i].Offs.Start < mergedNode.Chldren[j].Offs.Start
+		})
+		return mergedNode
+	}
+
+	// Узлы не пересекаются — создаем родительский узел
+	parentNode := &Node{
+		Offs: Offset{
+			Start: min(node1.Offs.Start, node2.Offs.Start),
+			End:   max(node1.Offs.End, node2.Offs.End),
+		},
+		Chldren: []*Node{node1, node2},
+	}
+	//parentNode.Children.Sort((a, b) => a.Offset.Start.CompareTo(b.Offset.Start));
+	sort.Slice(parentNode.Chldren, func(i, j int) bool {
+		return parentNode.Chldren[i].Offs.Start < parentNode.Chldren[j].Offs.Start
+	})
+	return parentNode
+}
+
+func MergeChildLists(list1, list2 []*Node) []*Node {
+	mergedList := make([]*Node, 0, len(list1)+len(list2))
+	i, j := 0, 0
+
+	for i < len(list1) && j < len(list2) {
+		child1 := list1[i]
+		child2 := list2[j]
+
+		if child1.Offs == child2.Offs {
+			mergedList = append(mergedList, MergeTrees(child1, child2))
+			i++
+			j++
+		} else if child1.Offs.Start < child2.Offs.Start {
+			mergedList = append(mergedList, child1)
+			i++
+		} else {
+			mergedList = append(mergedList, child2)
+			j++
+		}
+	}
+
+	if i < len(list1) {
+		mergedList = append(mergedList, list1[i:]...)
+	}
+
+	if j < len(list2) {
+		mergedList = append(mergedList, list2[i:]...)
+	}
+
+	return mergedList
+}
+
+func MergeChildIntoChildren(children []*Node, childToMerge *Node) []*Node {
+	merged := false
+	for i, child := range children {
+		if child.Offs.Contains(childToMerge.Offs) || child.Offs == childToMerge.Offs {
+			children[i] = MergeTrees(child, childToMerge)
+			merged = true
+			break
+		}
+	}
+	if !merged {
+		children = append(children, childToMerge)
+	}
+
+	//children.Sort((a, b) => a.Offset.Start.CompareTo(b.Offset.Start));
+	sort.Slice(children, func(i, j int) bool {
+		return children[i].Offs.Start < children[j].Offs.Start
+	})
+	return children
+}
---
+++ GoExample/ast/node_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_MergeTrees(t *testing.T) {
+	tests := []struct {
+		n1, n2, res *Node
+	}{
+		{ // n2 внутри n1
+			n1:  &Node{Offs: Offset{2, 20}},
+			n2:  &Node{Offs: Offset{5, 10}},
+			res: &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{5, 10}}}},
+		},
+		{ // n2 внутри n1
+			n1:  &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{5, 10}}}},
+			n2:  &Node{Offs: Offset{15, 19}},
+			res: &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{5, 10}}, {Offs: Offset{15, 19}}}},
+		},
+		{ // n2 внутри n1
+			n1:  &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{15, 19}}}},
+			n2:  &Node{Offs: Offset{5, 10}},
+			res: &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{5, 10}}, {Offs: Offset{15, 19}}}},
+		},
+		{ // n2 внутри n1
+			n1:  &Node{Offs: Offset{5, 20}, Chldren: []*Node{{Offs: Offset{15, 20}}}},
+			n2:  &Node{Offs: Offset{5, 10}},
+			res: &Node{Offs: Offset{5, 20}, Chldren: []*Node{{Offs: Offset{5, 10}}, {Offs: Offset{15, 20}}}},
+		},
+		{ // n2 внутри n1
+			n1:  &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{3, 7}}, {Offs: Offset{15, 19}}}},
+			n2:  &Node{Offs: Offset{8, 12}},
+			res: &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{3, 7}}, {Offs: Offset{8, 12}}, {Offs: Offset{15, 19}}}},
+		},
+		{ // n2 внутри n1 матрешкой
+			n1:  &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{5, 15}}}},
+			n2:  &Node{Offs: Offset{6, 12}},
+			res: &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{5, 15}, Chldren: []*Node{{Offs: Offset{6, 12}}}}}},
+		},
+		{ // n2 внутри n1 матрешкой
+			n1:  &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{2, 4}}, {Offs: Offset{5, 15}}, {Offs: Offset{15, 19}}}},
+			n2:  &Node{Offs: Offset{6, 12}},
+			res: &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{2, 4}}, {Offs: Offset{5, 15}, Chldren: []*Node{{Offs: Offset{6, 12}}}}, {Offs: Offset{15, 19}}}},
+		},
+	}
+
+	for _, tt := range tests {
+		res := MergeTrees(tt.n1, tt.n2)
+		assert.EqualValues(t, tt.res, res)
+	}
+}
