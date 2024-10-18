e1c7e685c114a5181b83cae27ac809c727b3e5e8
Дроздов Дмитрий Сергеевич <dsdrozd2@mts.ru>
2024-10-18 15:51:15 +0300

    tests

--- GoExample/ast/node.go
+++ GoExample/ast/node.go
@@ -2,21 +2,21 @@ package main
 
 import "sort"
 
-type Offset struct {
+type Shift struct {
 	Start int
 	End   int
 }
 
-func (o Offset) Contains(other Offset) bool {
+func (o Shift) Contains(other Shift) bool {
 	return o.Start <= other.Start && o.End >= other.End
 }
 
-func (o Offset) Overlaps(other Offset) bool {
+func (o Shift) Overlaps(other Shift) bool {
 	return o.Start < other.End && o.End > other.Start
 }
 
 type Node struct {
-	Offs    Offset
+	Shft    Shift
 	Chldren []*Node
 }
 
@@ -28,50 +28,50 @@ func MergeTrees(node1, node2 *Node) *Node {
 		return node1
 	}
 
-	if node1.Offs == node2.Offs {
+	if node1.Shft == node2.Shft {
 		return &Node{
-			Offs:    node1.Offs,
+			Shft:    node1.Shft,
 			Chldren: MergeChildLists(node1.Chldren, node2.Chldren),
 		}
 	}
 
-	if node1.Offs.Contains(node2.Offs) {
+	if node1.Shft.Contains(node2.Shft) {
 		node1.Chldren = MergeChildIntoChildren(node1.Chldren, node2)
 		return node1
 	}
 
-	if node2.Offs.Contains(node1.Offs) {
+	if node2.Shft.Contains(node1.Shft) {
 		node2.Chldren = MergeChildIntoChildren(node2.Chldren, node1)
 		return node2
 	}
 
 	// Обработка перекрывающихся узлов
-	if node1.Offs.Overlaps(node2.Offs) {
+	if node1.Shft.Overlaps(node2.Shft) {
 		mergedNode := &Node{
-			Offs: Offset{
-				Start: min(node1.Offs.Start, node2.Offs.Start),
-				End:   max(node1.Offs.End, node2.Offs.End),
+			Shft: Shift{
+				Start: min(node1.Shft.Start, node2.Shft.Start),
+				End:   max(node1.Shft.End, node2.Shft.End),
 			},
 			Chldren: []*Node{node1, node2},
 		}
 		//mergedNode.Children.Sort((a, b) => a.Offset.Start.CompareTo(b.Offset.Start));
 		sort.Slice(mergedNode.Chldren, func(i, j int) bool {
-			return mergedNode.Chldren[i].Offs.Start < mergedNode.Chldren[j].Offs.Start
+			return mergedNode.Chldren[i].Shft.Start < mergedNode.Chldren[j].Shft.Start
 		})
 		return mergedNode
 	}
 
 	// Узлы не пересекаются — создаем родительский узел
 	parentNode := &Node{
-		Offs: Offset{
-			Start: min(node1.Offs.Start, node2.Offs.Start),
-			End:   max(node1.Offs.End, node2.Offs.End),
+		Shft: Shift{
+			Start: min(node1.Shft.Start, node2.Shft.Start),
+			End:   max(node1.Shft.End, node2.Shft.End),
 		},
 		Chldren: []*Node{node1, node2},
 	}
 	//parentNode.Children.Sort((a, b) => a.Offset.Start.CompareTo(b.Offset.Start));
 	sort.Slice(parentNode.Chldren, func(i, j int) bool {
-		return parentNode.Chldren[i].Offs.Start < parentNode.Chldren[j].Offs.Start
+		return parentNode.Chldren[i].Shft.Start < parentNode.Chldren[j].Shft.Start
 	})
 	return parentNode
 }
@@ -84,11 +84,11 @@ func MergeChildLists(list1, list2 []*Node) []*Node {
 		child1 := list1[i]
 		child2 := list2[j]
 
-		if child1.Offs == child2.Offs {
+		if child1.Shft == child2.Shft {
 			mergedList = append(mergedList, MergeTrees(child1, child2))
 			i++
 			j++
-		} else if child1.Offs.Start < child2.Offs.Start {
+		} else if child1.Shft.Start < child2.Shft.Start {
 			mergedList = append(mergedList, child1)
 			i++
 		} else {
@@ -102,7 +102,7 @@ func MergeChildLists(list1, list2 []*Node) []*Node {
 	}
 
 	if j < len(list2) {
-		mergedList = append(mergedList, list2[i:]...)
+		mergedList = append(mergedList, list2[j:]...)
 	}
 
 	if len(mergedList) == 0 {
@@ -115,7 +115,7 @@ func MergeChildLists(list1, list2 []*Node) []*Node {
 func MergeChildIntoChildren(children []*Node, childToMerge *Node) []*Node {
 	merged := false
 	for i, child := range children {
-		if child.Offs.Contains(childToMerge.Offs) || child.Offs == childToMerge.Offs {
+		if child.Shft.Contains(childToMerge.Shft) || child.Shft == childToMerge.Shft {
 			children[i] = MergeTrees(child, childToMerge)
 			merged = true
 			break
@@ -127,7 +127,7 @@ func MergeChildIntoChildren(children []*Node, childToMerge *Node) []*Node {
 
 	//children.Sort((a, b) => a.Offset.Start.CompareTo(b.Offset.Start));
 	sort.Slice(children, func(i, j int) bool {
-		return children[i].Offs.Start < children[j].Offs.Start
+		return children[i].Shft.Start < children[j].Shft.Start
 	})
 	return children
 }
--- GoExample/ast/node_test.go
+++ GoExample/ast/node_test.go
@@ -10,45 +10,60 @@ func Test_MergeTrees(t *testing.T) {
 	tests := []struct {
 		n1, n2, res *Node
 	}{
+		{ // equals
+			n1:  &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{5, 10}}}},
+			n2:  &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{11, 15}}}},
+			res: &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{5, 10}}, {Shft: Shift{11, 15}}}},
+		},
+		{ // equals
+			n1:  &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{5, 10}}, {Shft: Shift{11, 15}}}},
+			n2:  &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{11, 15}}, {Shft: Shift{16, 20}}}},
+			res: &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{5, 10}}, {Shft: Shift{11, 15}}, {Shft: Shift{16, 20}}}},
+		},
+		{ // equals + merge
+			n1:  &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{5, 10}}, {Shft: Shift{11, 15}, Chldren: []*Node{{Shft: Shift{11, 13}}}}}},
+			n2:  &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{11, 15}, Chldren: []*Node{{Shft: Shift{14, 15}}}}, {Shft: Shift{16, 20}}}},
+			res: &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{5, 10}}, {Shft: Shift{11, 15}, Chldren: []*Node{{Shft: Shift{11, 13}}, {Shft: Shift{14, 15}}}}, {Shft: Shift{16, 20}}}},
+		},
 		{ // nil check
-			n1:  &Node{Offs: Offset{2, 20}},
+			n1:  &Node{Shft: Shift{2, 20}},
 			n2:  nil,
-			res: &Node{Offs: Offset{2, 20}},
+			res: &Node{Shft: Shift{2, 20}},
 		},
 		{ // n2 внутри n1
-			n1:  &Node{Offs: Offset{2, 20}},
-			n2:  &Node{Offs: Offset{5, 10}},
-			res: &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{5, 10}}}},
+			n1:  &Node{Shft: Shift{2, 20}},
+			n2:  &Node{Shft: Shift{5, 10}},
+			res: &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{5, 10}}}},
 		},
 		{ // n2 внутри n1
-			n1:  &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{5, 10}}}},
-			n2:  &Node{Offs: Offset{15, 19}},
-			res: &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{5, 10}}, {Offs: Offset{15, 19}}}},
+			n1:  &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{5, 10}}}},
+			n2:  &Node{Shft: Shift{15, 19}},
+			res: &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{5, 10}}, {Shft: Shift{15, 19}}}},
 		},
 		{ // n2 внутри n1
-			n1:  &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{15, 19}}}},
-			n2:  &Node{Offs: Offset{5, 10}},
-			res: &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{5, 10}}, {Offs: Offset{15, 19}}}},
+			n1:  &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{15, 19}}}},
+			n2:  &Node{Shft: Shift{5, 10}},
+			res: &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{5, 10}}, {Shft: Shift{15, 19}}}},
 		},
 		{ // n2 внутри n1
-			n1:  &Node{Offs: Offset{5, 20}, Chldren: []*Node{{Offs: Offset{15, 20}}}},
-			n2:  &Node{Offs: Offset{5, 10}},
-			res: &Node{Offs: Offset{5, 20}, Chldren: []*Node{{Offs: Offset{5, 10}}, {Offs: Offset{15, 20}}}},
+			n1:  &Node{Shft: Shift{5, 20}, Chldren: []*Node{{Shft: Shift{15, 20}}}},
+			n2:  &Node{Shft: Shift{5, 10}},
+			res: &Node{Shft: Shift{5, 20}, Chldren: []*Node{{Shft: Shift{5, 10}}, {Shft: Shift{15, 20}}}},
 		},
 		{ // n2 внутри n1
-			n1:  &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{3, 7}}, {Offs: Offset{15, 19}}}},
-			n2:  &Node{Offs: Offset{8, 12}},
-			res: &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{3, 7}}, {Offs: Offset{8, 12}}, {Offs: Offset{15, 19}}}},
+			n1:  &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{3, 7}}, {Shft: Shift{15, 19}}}},
+			n2:  &Node{Shft: Shift{8, 12}},
+			res: &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{3, 7}}, {Shft: Shift{8, 12}}, {Shft: Shift{15, 19}}}},
 		},
 		{ // n2 внутри n1 матрешкой
-			n1:  &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{5, 15}}}},
-			n2:  &Node{Offs: Offset{6, 12}},
-			res: &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{5, 15}, Chldren: []*Node{{Offs: Offset{6, 12}}}}}},
+			n1:  &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{5, 15}}}},
+			n2:  &Node{Shft: Shift{6, 12}},
+			res: &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{5, 15}, Chldren: []*Node{{Shft: Shift{6, 12}}}}}},
 		},
 		{ // n2 внутри n1 матрешкой
-			n1:  &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{2, 4}}, {Offs: Offset{5, 15}}, {Offs: Offset{15, 19}}}},
-			n2:  &Node{Offs: Offset{6, 12}},
-			res: &Node{Offs: Offset{2, 20}, Chldren: []*Node{{Offs: Offset{2, 4}}, {Offs: Offset{5, 15}, Chldren: []*Node{{Offs: Offset{6, 12}}}}, {Offs: Offset{15, 19}}}},
+			n1:  &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{2, 4}}, {Shft: Shift{5, 15}}, {Shft: Shift{15, 19}}}},
+			n2:  &Node{Shft: Shift{6, 12}},
+			res: &Node{Shft: Shift{2, 20}, Chldren: []*Node{{Shft: Shift{2, 4}}, {Shft: Shift{5, 15}, Chldren: []*Node{{Shft: Shift{6, 12}}}}, {Shft: Shift{15, 19}}}},
 		},
 	}
 
